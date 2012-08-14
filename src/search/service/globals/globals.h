581faf3e9dbf361030a3dd64a6799d78dbdf0dfe
Julian Kranz <julian@juliankranz.de>
2012-08-14 19:56:48 +0200

    Gnunet Service Search
    - Documentation

--- src/search/service/globals/globals.h
+++ src/search/service/globals/globals.h
@@ -1,8 +1,26 @@
-/*
- * globals.h
+/**
+ * @file search/service/globals/globals.h
+ * @author Julian Kranz
+ * @date 7.8.2012
  *
- *  Created on: Aug 7, 2012
- *      Author: jucs
+ * @brief This file defines all exported data structures, functions, constants and variables pertaining to
+ * the GNUnet Search service's flooding component.
+ */
+/*
+ *  This file is part of GNUnet Search.
+ *
+ *  GNUnet Search is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GNUnet Search is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNUnet Search.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GLOBALS_H_
