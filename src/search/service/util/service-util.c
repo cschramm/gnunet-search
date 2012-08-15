fc08dae65fff4dedd6d17879d8aa2ee12d191ec3
Julian Kranz <julian@juliankranz.de>
2012-08-15 16:14:23 +0200

    Gnunet Service Search
    - Documentation

--- src/search/service/util/service-util.c
+++ src/search/service/util/service-util.c
@@ -1,8 +1,25 @@
-/*
- * gnunet-search-util.c
+/**
+ * @file search/service/util/service-util.c
+ * @author Julian Kranz
+ * @date 3.8.2012
  *
- *  Created on: Aug 3, 2012
- *      Author: jucs
+ * @brief This file contains all functions pertaining to the GNUnet Search service's utility component.
+ */
+/*
+ *  This file is part of GNUnet Search.
+ *
+ *  GNUnet Search is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GNUnet Search is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNUnet Search.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #define _GNU_SOURCE
@@ -15,6 +32,13 @@
 #include <gnunet/gnunet_util_lib.h>
 #include <gnunet/gnunet_dht_service.h>
 
+/**
+ * @brief This function generates a structured DHT key or value.
+ *
+ * @param key_value a reference to a memory location used to store the resulting DHT key or value
+ * @param action an action associated with key or the value (not relevant anymore)
+ * @param data the data to be included in the key or value
+ */
 void gnunet_search_util_key_value_generate_simple(char **key_value, const char *action, const char *data) {
 	size_t key_value_length;
 	FILE *key_value_stream = open_memstream(key_value, &key_value_length);
@@ -25,8 +49,12 @@ void gnunet_search_util_key_value_generate_simple(char **key_value, const char *
 	fclose(key_value_stream);
 }
 
-/*
- * Security: precond: size > sizeof(struct search_command)
+/**
+ * @brief This function extracts a keyword using a search command structure.
+ *
+ * @param keyword a reference to a memory location used to store the keyword
+ * @param cmd a reference to the search command
+ * @param size the amount of memory available including the search command; the caller has to make that this size is at least sizeof(struct search_command).
  */
 void gnunet_search_util_cmd_keyword_get(char **keyword, struct search_command const *cmd, size_t size) {
 	size_t max_length = size - sizeof(struct search_command);
--- src/search/service/util/service-util.h
+++ src/search/service/util/service-util.h
@@ -1,8 +1,26 @@
-/*
- * gnunet-search-util.h
+/**
+ * @file search/service/util/service-util.h
+ * @author Julian Kranz
+ * @date 3.8.2012
  *
- *  Created on: Aug 3, 2012
- *      Author: jucs
+ * @brief This file defines all exported data structures, functions, constants and variables pertaining to
+ * the GNUnet Search service's utility component.
+ */
+/*
+ *  This file is part of GNUnet Search.
+ *
+ *  GNUnet Search is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GNUnet Search is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNUnet Search.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GNUNET_SEARCH_UTIL_H_
